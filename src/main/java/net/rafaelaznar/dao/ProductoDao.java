b5b059b1ff168ba77ce0209d0b3fcc141e87a66b
mati <mati@s09-pc05>
2014-01-09 10:12:09 +0100

    Mantenimiento de producto completado

---
+++ src/main/java/net/rafaelaznar/dao/ProductoDao.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.rafaelaznar.dao;
+
+import net.rafaelaznar.bean.ProductoBean;
+
+
+/**
+ *
+ * @author Javier Bonet
+ */
+public interface ProductoDao extends GenericDao<ProductoBean>{
+    
+}
